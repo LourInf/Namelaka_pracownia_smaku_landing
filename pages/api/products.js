40636b98543e96b30b9367031311b62185b9f2d6
LourInf <lourdes.infante@gmail.com>
2024-04-29 11:05:20 +0200

    feat: filters

---
+++ pages/api/products.js
@@ -0,0 +1,22 @@
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/Product";
+
+//This handler processes GET requests where query parameters = categories to filter by + any other filters applicable to the properties field of the products. It constructs a query object based on these parameters and fetches the relevant products from the database.
+
+export default async function handler(req, res) {
+  await mongooseConnect();
+  const { categories, ...filters } = req.query; // Destructure 'categories' and the rest of the filters from the request query parameters
+  const queryObject = { category: { $in: categories.split(",") } }; // Create a query object to find products with categories that match any in the provided list
+  // For each filter, use dot notation to query inside the properties object
+  // Iterate over each filter in the filters object
+  Object.keys(filters).forEach((key) => {
+    // If the filter value isn't 'all' (all=no filters)
+    // add a property to the query object to filter by this key within the 'properties' object of the Product schema
+    if (filters[key] !== "all") {
+      queryObject[`properties.${key}`] = filters[key];
+    }
+  });
+
+  //   console.log("Query Object:", queryObject);
+  res.json(await Product.find(queryObject)); // Use the Product model to find all products that match the query object and return them as JSON
+}
--- pages/category/[id].js
+++ pages/category/[id].js
@@ -2,11 +2,80 @@ import { Category } from "@/models/Category";
 import Products from "../products";
 import { Product } from "@/models/Product";
 import ProductCard from "@/components/ProductCard";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function CategoryPage({ category, products }) {
+export default function CategoryPage({
+  category,
+  subCategories,
+  products: originalProducts,
+}) {
+  //the initial state should be an object where we have as key the property name and  as value "all"
+  const [products, setProducts] = useState(originalProducts);
+  const [filtersValues, setFiltersValues] = useState(
+    category.properties.map((p) => ({
+      name: p.name,
+      value: "all",
+    }))
+  );
+  //   console.log("filtersValues: ", filtersValues);
+
+  function handleFilterChange(filterName, filterValue) {
+    setFiltersValues((prev) => {
+      const newValue = prev.map((p) => ({
+        name: p.name,
+        value: p.name === filterName ? filterValue : p.value,
+      }));
+      return newValue;
+    });
+  }
+
+  useEffect(() => {
+    const catIds = [category._id, ...(subCategories?.map((c) => c._id) || [])];
+    // console.log(catIds);
+    const params = new URLSearchParams();
+    params.set("categories", catIds.join(","));
+    filtersValues.forEach((f) => {
+      if (f.value !== "all") {
+        params.set(f.name, f.value);
+      }
+    });
+    const url = `/api/products?` + params.toString();
+    axios.get(url).then((res) => {
+      //   console.log(res.data);
+      setProducts(res.data);
+    });
+  }, [filtersValues]);
+
   return (
     <>
       <h1 className="text-2xl text-center mt-60 mb-4">{category?.name}</h1>
+      <div className="flex flex-row gap-6 justify-center mb-4">
+        {category && category.properties && category.properties.length > 0 ? (
+          category.properties.map((prop) => (
+            <div key={prop.name} className="bg-slate-300 rounded-md pl-2">
+              <label htmlFor={prop.name}>{prop.name}:</label>
+              <select
+                id={prop.name}
+                value={filtersValues.find((f) => f.name === prop.name).value}
+                onChange={(e) => handleFilterChange(prop.name, e.target.value)}
+                className="bg-transparent rounded-md border-none"
+              >
+                <option value="all" className="bg-slate-200">
+                  Show all
+                </option>
+                {prop.values.map((val) => (
+                  <option key={val} value={val} className="bg-slate-200">
+                    {val}
+                  </option>
+                ))}
+              </select>
+            </div>
+          ))
+        ) : (
+          <p>No filters available.</p>
+        )}
+      </div>
       <div className="grid grid-cols-5 gap-4">
         {products &&
           products.length > 0 &&
@@ -21,13 +90,16 @@ export default function CategoryPage({ category, products }) {
 
 export async function getServerSideProps(context) {
   const category = await Category.findById(context.query.id);
+  //   console.log("Category data:", category);
   const subCategories = await Category.find({ parent: category._id });
   const catIds = [category._id, ...subCategories.map((c) => c._id)];
   const products = await Product.find({ category: catIds });
+  //   console.log("Products data:", products);
   return {
     props: {
       category: JSON.parse(JSON.stringify(category)),
       products: JSON.parse(JSON.stringify(products)),
+      subCategories: JSON.parse(JSON.stringify(subCategories)),
     },
   };
 }
